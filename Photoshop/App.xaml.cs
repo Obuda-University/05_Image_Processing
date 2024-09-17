f54a4f65f8fd1e538c2572bfe2f83aab95998bdf
Gilgameshh73 <agostonandras51@gmail.com>
2024-09-17 23:03:52 +0200

    Add project files.

---
+++ Photoshop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Photoshop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
