b43c012ea5e5c1325b0062a5cbc391c47ed1b7a1
Gilgameshh73 <agostonandras51@gmail.com>
2024-09-26 10:49:27 +0200

    Separate toolbar into another class.

--- Photoshop/main.py
+++ Photoshop/main.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMenu, QToolBar, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMenu, QWidget, QLabel, QPushButton, QVBoxLayout
+from CustomToolBar import CustomToolBar
 
 
 class PhotoshopApplication(QMainWindow):
@@ -9,35 +10,22 @@ class PhotoshopApplication(QMainWindow):
         self.WINDOW_WIDTH: int = 1000
         self.WINDOW_HEIGHT: int = 800
         self.WINDOW_TITLE: str = "Photoshop App"
+        self.WINDOW_ICON: str = "Resources\\icon2.png"
 
         self.setWindowTitle(self.WINDOW_TITLE)
         self.resize(self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
+        self.setWindowIcon(QIcon(self.WINDOW_ICON))
 
         self.create_toolbar()
 
     def create_toolbar(self) -> None:
-        toolbar = QToolBar("Main Toolbar")
-        self.addToolBar(toolbar)
+        main_toolbar = CustomToolBar(self)
+        self.addToolBar(main_toolbar)
 
-        menu = QMenu("File Actions", self)
-        label = QLabel("Label", self)
-        toolbar.addWidget(label)
-        file_menu_action = QAction("File Actions", self)
-        file_menu_action.setMenu(menu)
-        toolbar.addAction(file_menu_action)
-
-        new_action = QAction("New", self)
-        open_action = QAction("Open", self)
-        save_action = QAction("Save", self)
-
-        menu.addAction(new_action)
-        menu.addAction(open_action)
-        toolbar.addAction(save_action)
-
-        new_action.triggered.connect(self.new_file)
+        """new_action.triggered.connect(self.new_file)
         open_action.triggered.connect(self.open_file)
         save_action.triggered.connect(self.save_file)
-        file_menu_action.triggered.connect(self.action)
+        file_menu_action.triggered.connect(self.action)"""
 
     def action(self) -> None:
         print("asd")
