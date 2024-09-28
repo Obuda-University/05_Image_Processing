a83cc5e76b482cab108c05e751ae51091237b0d7
Gilgameshh73 <agostonandras51@gmail.com>
2024-09-28 17:43:22 +0200

    Adjust functions to check minimum and maximum limits.

--- Photoshop/CustomView.py
+++ Photoshop/CustomView.py
@@ -1,8 +1,11 @@
 from PyQt6.QtWidgets import QGraphicsView
 from PyQt6.QtGui import QPainter, QWheelEvent
+from PyQt6.QtCore import pyqtSignal
 
 
 class CustomView(QGraphicsView):
+    zoom_changed = pyqtSignal(float)  # Signal to emit the new zoom factor
+
     def __init__(self, *args, **kwargs) -> None:
         super(CustomView, self).__init__(*args, **kwargs)
         self.setRenderHint(QPainter.RenderHint.Antialiasing)
@@ -22,17 +25,27 @@ class CustomView(QGraphicsView):
 
     def zoom_in(self) -> None:
         """Scales the window up"""
-        self.scale(1.1, 1.1)
-        self.scale_factor *= 1.1
+        if self.scale_factor < 8.0:  # Upper limit: 800%
+            self.scale(1.1, 1.1)
+            self.scale_factor *= 1.1
+            if self.scale_factor > 8.0:
+                self.scale_factor = 8.0
+            self.zoom_changed.emit(self.scale_factor)
 
     def zoom_out(self) -> None:
         """Scales the window down"""
-        self.scale(0.9, 0.9)
-        self.scale_factor *= 0.9
+        if self.scale_factor > 0.1:  # Lower limit: 10%
+            self.scale(0.9, 0.9)
+            self.scale_factor *= 0.9
+            if self.scale_factor < 0.1:
+                self.scale_factor = 0.1
+            self.zoom_changed.emit(self.scale_factor)
 
     def set_zoom(self, factor: float) -> None:
         """Set the zoom with a specific factor"""
-        self.resetTransform()
-        self.scale(factor, factor)
-        self.scale_factor = factor
+        if 0.1 <= factor <= 8.0:
+            self.resetTransform()
+            self.scale(factor, factor)
+            self.scale_factor = factor
+            self.zoom_changed.emit(self.scale_factor)
     
