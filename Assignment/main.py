ec5ae7c830cd5c2f1ecb09e1042995187e7ebdf6
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-05 17:01:05 +0200

    Hand detection.

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,4 +1,6 @@
+from cvzone.HandTrackingModule import HandDetector
 import cv2
+
 
 cap = cv2.VideoCapture(0)  # ID: 0 | Webcam ID
 
@@ -6,8 +8,19 @@ cap = cv2.VideoCapture(0)  # ID: 0 | Webcam ID
 cap.set(3, 1280)
 cap.set(4, 720)
 
+detector = HandDetector(detectionCon=0.8)  # To be more accurate
+
 while True:
     success, img = cap.read()
+
+    hands, img = detector.findHands(img)  # hands: List of Hand Landmarks
+
+    if hands:
+        hand = hands[0]
+
+        landmark_list = hand["lmList"]
+        bounding_box_info = hand["bbox"]
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
