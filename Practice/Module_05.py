76d34f4941dc29de8b93f15094390f6697dfc7d1
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-06 21:57:34 +0200

    Solve second task.

--- Practice/Module_05.py
+++ Practice/Module_05.py
@@ -36,7 +36,34 @@ plt.show()
 # endregion
 
 # region Task 2
+binary_image = np.zeros((10, 10), dtype=np.uint8)
+binary_image[3:7, 3:7] = 1
 
+# Apply Sobel Operator
+sobel_x = cv2.Sobel(binary_image, cv2.CV_64F, 1, 0, ksize=3)
+sobel_y = cv2.Sobel(binary_image, cv2.CV_64F, 0, 1, ksize=3)
+
+# Gradient magnitude and direction
+gradient_magnitude = np.sqrt(sobel_x ** 2 + sobel_y ** 2)
+gradient_direction = np.arctan2(sobel_y, sobel_x) * (180 / np.pi)
+
+# Show Results
+plt.subplot(1, 3, 1)
+plt.imshow(binary_image, cmap='gray')
+plt.title('Binary Image')
+plt.colorbar()
+
+plt.subplot(1, 3, 2)
+plt.imshow(gradient_direction, cmap='hsv')
+plt.title('Gradient Direction')
+plt.colorbar()
+
+plt.subplot(1, 3, 3)
+plt.imshow(gradient_magnitude, cmap='gray')
+plt.title('Gradient Magnitude')
+plt.colorbar()
+
+plt.show()
 # endregion
 
 # region Task 3
