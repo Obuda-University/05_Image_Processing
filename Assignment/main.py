6b6fede3130e28022b26567170806617782b5194
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-07 23:05:53 +0200

    Export handtracking to a separate class.

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,22 +1,9 @@
-from cvzone.HandTrackingModule import HandDetector
 from pynput.keyboard import Controller, Key
 from Keyboard import KeyBoard
 import concurrent.futures
 import numpy as np
 import threading
 import cv2
-
-
-
-
-
-class HandTracking:
-    def __init__(self, max_hands: int = 4, detection_confidence: float = 0.8) -> None:
-        self.detector = HandDetector(staticMode=False, maxHands=max_hands, detectionCon=detection_confidence)
-
-    def detect_hands(self, frame: any) -> tuple[list, np.ndarray]:
-        hands, frame = self.detector.findHands(frame, flipType=False)
-        return hands, frame
 
 
 class VirtualKeyboard:
