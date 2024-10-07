c27ee4e1ea76a3f178f51cbb6638534e4a75c382
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-07 23:07:59 +0200

    Export virtual keyboard to a separate class.

--- Assignment/main.py
+++ Assignment/main.py
@@ -1,42 +1,8 @@
-from pynput.keyboard import Controller, Key
-from Keyboard import KeyBoard
+from VirtualKeyboard import VirtualKeyboard
+from HandTracking import HandTracking
+from Camera import Camera
 import concurrent.futures
-import numpy as np
-import threading
 import cv2
-
-
-class VirtualKeyboard:
-    def __init__(self, layout: list[list[str]], hand_tracking: HandTracking) -> None:
-        self.keyboard = KeyBoard(layout)
-        self.keyboard_controller = Controller()
-        self.hand_tracking = hand_tracking
-
-    def process_input(self, hands: list) -> None:
-        if hands:
-            hand = hands[0]
-            landmark_list = hand["lmList"]
-            x_finger, y_finger = landmark_list[8][:2]  # Tip of right index finger
-            pressed_key = self.keyboard.check_pressed_key(x_finger, y_finger)
-
-            if pressed_key:
-                self.handle_key_press(pressed_key, landmark_list)
-
-    def handle_key_press(self, key, landmark_list: list) -> None:
-        l, _, _ = self.hand_tracking.detector.findDistance(landmark_list[8][:2], landmark_list[4][:2], None)
-        if l < 25:
-            print(key)
-            if key == 'space':
-                self.keyboard_controller.press(Key.space)
-            elif key == 'OK':
-                self.keyboard_controller.press(Key.enter)
-            else:
-                self.keyboard_controller.press(key)
-            # Use threading to release the key after a delay without blocking main execution
-            threading.Thread(target=self.keyboard_controller.release, args=[key]).start()
-
-    def draw(self, frame: np.ndarray) -> np.ndarray:
-        return self.keyboard.draw_keyboard(frame)
 
 
 class Application:
