34ec7f209b6862aee14360c3924ad058d717f664
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-15 19:00:56 +0200

    Add cursor movement (need to optimize).

--- Assignment/VirtualMouse.py
+++ Assignment/VirtualMouse.py
@@ -1,11 +1,12 @@
+from Assignment.HandTracking import HandTracking
 import numpy as np
 import time
 import cv2
 
 
 class VirtualMouse:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tracker: HandTracking) -> None:
+        self.tracker = tracker
 
     # TODO: Get the fingers [predefined gesture]
     # TODO: Check the gesture
--- Assignment/main.py
+++ Assignment/main.py
@@ -3,8 +3,12 @@ from HandTracking import HandTracking
 from Camera import Camera
 import concurrent.futures
 import numpy as np
+import pyautogui
 import time
 import cv2
+
+screen_width, screen_height = pyautogui.size()
+camera_width, camera_height = 640, 480
 
 
 class Application:
@@ -25,6 +29,24 @@ class Application:
 
                     hands_future = executor.submit(self.hand_tracking.detect_hands, frame)
                     hands, processed_frame = hands_future.result()
+
+
+                    if hands:
+                        hand = hands[0]
+                        landmark_list = hand["lmList"]
+
+                        x1, y1 = landmark_list[8][1:]  # Index finger
+                        x2, y2 = landmark_list[12][1:]  # Middle finger
+
+                        fingers = self.hand_tracking.detector.fingersUp(hand)
+
+                        # Index finger up, middle finger down
+                        if fingers[1] == 1 and fingers[2] == 0:
+                            x3 = np.interp(x1, (0, camera_width), (0, screen_width))
+                            y3 = np.interp(y1, (0, camera_height), (0, screen_height))
+
+                            cv2.circle(processed_frame, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+                            pyautogui.move(x3, y3)
 
                     input_future = executor.submit(self.onscreen_keyboard.key_press, hands)
                     input_future.result()
