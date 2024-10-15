460ba5f9bcd3a2417f8252189e4645c6acd15167
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-15 17:23:03 +0200

    Set max detectable hands to 2.

--- Assignment/HandTracking.py
+++ Assignment/HandTracking.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class HandTracking:
-    def __init__(self, max_hands: int = 4, detection_confidence: float = 0.8) -> None:
+    def __init__(self, max_hands: int = 2, detection_confidence: float = 0.8) -> None:
         self.detector = HandDetector(staticMode=False, maxHands=max_hands, detectionCon=detection_confidence)
 
     def detect_hands(self, frame: any) -> tuple[list, np.ndarray]:
