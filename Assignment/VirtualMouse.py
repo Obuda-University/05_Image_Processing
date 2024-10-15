905505ae45cbbd234dc01ce98c63a086075c238d
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-15 14:54:19 +0200

    Add class for virtual mouse.

---
+++ Assignment/VirtualMouse.py
@@ -0,0 +1,18 @@
+import numpy as np
+import time
+import cv2
+
+
+class VirtualMouse:
+    def __init__(self) -> None:
+        pass
+
+    # TODO: Get the fingers [predefined gesture]
+    # TODO: Check the gesture
+    # TODO: Moving mode
+    # TODO: Convert coordinates
+    # TODO: Smoothen values
+    # TODO: Move mouse
+    # TODO: Clicking mode [predefined gesture]
+    # TODO: Find distance [for gesture checking]
+    # TODO: Click mouse
