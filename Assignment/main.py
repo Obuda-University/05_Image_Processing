46d4cab778b027dbea45159231c8d3533bb991a9
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-15 14:51:13 +0200

    Add fps counter in the top-left corner.

--- Assignment/main.py
+++ Assignment/main.py
@@ -2,6 +2,8 @@ from OnScreenKeyboard import OnScreenKeyboard
 from HandTracking import HandTracking
 from Camera import Camera
 import concurrent.futures
+import numpy as np
+import time
 import cv2
 
 
@@ -11,6 +13,7 @@ class Application:
         self.hand_tracking = HandTracking()
         self.onscreen_keyboard = OnScreenKeyboard(self.hand_tracking.detector)
         self.running: bool = True
+        self._previous_time: int = 0
 
     def run(self) -> None:
         with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
@@ -18,6 +21,8 @@ class Application:
                 frame_future = executor.submit(self.camera.read_frame)
                 success, frame = frame_future.result()
                 if success:
+                    self.calc_frame_rate(frame)
+
                     hands_future = executor.submit(self.hand_tracking.detect_hands, frame)
                     hands, processed_frame = hands_future.result()
 
@@ -31,6 +36,12 @@ class Application:
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         self.running = False
         self.stop()
+
+    def calc_frame_rate(self, img: np.ndarray) -> None:
+        current_time: float = time.time()
+        fps: float = 1 / (current_time - self._previous_time)
+        self._previous_time = current_time
+        cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
 
     def stop(self) -> None:
         self.running = False
