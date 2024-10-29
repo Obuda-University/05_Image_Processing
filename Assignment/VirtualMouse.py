dffd44c45fa72a9a1f71a821db1400e9874c1688
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-29 14:29:07 +0100

    Setup class for the virtual mouse.

--- Assignment/VirtualMouse.py
+++ Assignment/VirtualMouse.py
@@ -1,12 +1,23 @@
-from Assignment.HandTracking import HandTracking
+from cvzone.HandTrackingModule import HandDetector
+import HandTracking
 import numpy as np
+import ctypes
 import time
 import cv2
 
 
 class VirtualMouse:
-    def __init__(self, tracker: HandTracking) -> None:
-        self.tracker = tracker
+    def __init__(self) -> None:
+        self.present_time = 0
+        self.CAMERA_WIDTH = 640
+        self.CAMERA_HEIGHT = 480
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.CAMERA_WIDTH)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.CAMERA_HEIGHT)
+        self.detector = HandDetector(staticMode=False, modelComplexity=0, maxHands=2, detectionCon=0.8, minTrackCon=0.5)
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.frame_reduction = 100
 
     # TODO: Get the fingers [predefined gesture]
     # TODO: Check the gesture
@@ -17,3 +28,7 @@ class VirtualMouse:
     # TODO: Clicking mode [predefined gesture]
     # TODO: Find distance [for gesture checking]
     # TODO: Click mouse
+
+if __name__ == '__main__':
+    app = VirtualMouse()
+    app.run()
