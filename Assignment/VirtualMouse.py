38ccf8dbce847fac6966d5791da7795046dbb45c
Gilgameshh73 <agostonandras51@gmail.com>
2024-10-31 21:03:17 +0100

    Improve code readability.

--- Assignment/VirtualMouse.py
+++ Assignment/VirtualMouse.py
@@ -5,41 +5,68 @@ import time
 import cv2
 
 # Constants
-CAMERA_WIDTH, CAMERA_HEIGHT = 640, 480
-SCREEN_WIDTH, SCREEN_HEIGHT = 1920, 1080
-FRAME_REDUCTION = 100
-SMOOTHENING_RATIO = 6
-CLICK_COOLDOWN = 0.3
-MOUSE_EVENT_LEFT_DOWN = 0x0002  # Left button down
-MOUSE_EVENT_LEFT_UP = 0x0004    # Left button up
+class Config:
+    CAMERA_WIDTH, CAMERA_HEIGHT = 640, 480
+    SCREEN_WIDTH, SCREEN_HEIGHT = 1920, 1080
+    FRAME_REDUCTION = 100
+    SMOOTHENING_RATIO = 6
+    CLICK_COOLDOWN = 0.3
+    MOUSE_EVENT_LEFT_DOWN = 0x0002  # Left button down
+    MOUSE_EVENT_LEFT_UP = 0x0004    # Left button up
 
 # Utils
-def _screen_coordinates(x, y, frame_reduction, cam_width, cam_height, screen_width, screen_height) -> [int, int]:
-    """Interpolate (convert) camera coordinates to screen coordinates"""
-    x_screen = int(np.interp(x, (frame_reduction, cam_width - frame_reduction), (0, screen_width)))
-    y_screen = int(np.interp(y, (frame_reduction, cam_height - frame_reduction), (0, screen_height)))
+def _screen_coordinates(x, y) -> [int, int]:
+    """Map camera coordinates to screen coordinates with frame reduction applied"""
+    x_screen = int(np.interp(x, (Config.FRAME_REDUCTION, Config.CAMERA_WIDTH - Config.FRAME_REDUCTION), (0, Config.SCREEN_WIDTH)))
+    y_screen = int(np.interp(y, (Config.FRAME_REDUCTION, Config.CAMERA_HEIGHT - Config.FRAME_REDUCTION), (0, Config.SCREEN_HEIGHT)))
     return x_screen, y_screen
 
-def _is_cooldown_elapsed(last_action_time: float, cooldown: float) -> bool:
+def _is_cooldown_elapsed(last_action_time: float, cooldown: float = Config.CLICK_COOLDOWN) -> bool:
     """Checks if the predefined cooldown period has elapsed since the last action"""
     return (time.time() - last_action_time) > cooldown
 
+def _click_event(down: bool = True) -> None:
+    """Perform mouse pressing action"""
+    event = Config.MOUSE_EVENT_LEFT_DOWN if down else Config.MOUSE_EVENT_LEFT_UP
+    try:
+        ctypes.windll.user32.mouse_event(event, 0, 0, 0, 0)
+    except Exception as e:
+        print(e)
 
-class _Click:
+class _ClickHandler:
     def __init__(self) -> None:
         self.thumb_prev_state = None
         self.last_click_time = 0
-        self.present_time = 0
         self.is_mouse_down = False
 
     def double(self) -> None:
-        ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_DOWN, 0, 0, 0, 0)
-        ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_UP, 0, 0, 0, 0)
-        ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_DOWN, 0, 0, 0, 0)
-        ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_UP, 0, 0, 0, 0)
-        self.last_click_time = self.present_time
+        """Simulate double click"""
+        _click_event(True)
+        _click_event(False)
+        _click_event(True)
+        _click_event(False)
+        self.last_click_time = time.time()
 
-    def single_hold(self, thumb_status: int) -> None:
+    def single(self) -> None:
+        """Simulate single click"""
+        _click_event(True)
+        _click_event(False)
+        self.last_click_time = time.time()
+
+    def hold(self) -> None:
+        """Simulate holding the left mouse button"""
+        if not self.is_mouse_down:
+            _click_event(True)
+            self.is_mouse_down = True
+
+    def release_hold(self) -> None:
+        """Release holding the left mouse button"""
+        if self.is_mouse_down:
+            _click_event(False)
+            self.is_mouse_down = False
+
+    def process_thumb_gesture(self, thumb_status: int) -> None:
+        """Decide single-click or hold based on the thumb status"""
         current_time = time.time()
 
         if self.thumb_prev_state is None:
@@ -50,15 +77,13 @@ class _Click:
             self.last_click_time = current_time
             self.is_mouse_down = False
         elif self.thumb_prev_state == 1 and thumb_status == 0:
-            if (current_time - self.last_click_time) <= CLICK_COOLDOWN:
-                ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_DOWN, 0, 0, 0, 0)
-                ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_UP, 0, 0, 0, 0)
+            if (current_time - self.last_click_time) <= Config.CLICK_COOLDOWN:
+                self.single()
             else:
-                ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_UP, 0, 0, 0, 0)
-        elif thumb_status == 1 and not self.is_mouse_down:
-            if (current_time - self.last_click_time) > CLICK_COOLDOWN:
-                ctypes.windll.user32.mouse_event(MOUSE_EVENT_LEFT_DOWN, 0, 0, 0, 0)
-                self.is_mouse_down = True
+                self.release_hold()
+        elif thumb_status == 1:
+            if _is_cooldown_elapsed(self.last_click_time):
+                self.hold()
         self.thumb_prev_state = thumb_status
 
 
@@ -66,13 +91,13 @@ class VirtualMouse:
     def __init__(self) -> None:
         self.time_now = 0
         self.cap = cv2.VideoCapture(0)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, Config.CAMERA_WIDTH)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, Config.CAMERA_HEIGHT)
         self.detector = HandDetector(staticMode=False, modelComplexity=0, maxHands=2, detectionCon=0.8, minTrackCon=0.5)
         self._prev_location_x, self._prev_location_y = 0, 0
         self._curr_location_x, self._curr_location_y = 0, 0
         self._last_click_time = 0
-        self._click = _Click()
+        self._click = _ClickHandler()
 
     def get_frame(self) -> [np.ndarray, None]:
         """Capture and flip the camera frame"""
@@ -88,21 +113,20 @@ class VirtualMouse:
 
     def _move_mouse(self, index_finger: list[int, int]) -> None:
         """Move mouse cursor based on the index finger's position"""
-        x_screen, y_screen = _screen_coordinates(index_finger[0], index_finger[1], FRAME_REDUCTION,
-                                                 CAMERA_WIDTH, CAMERA_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT)
-        self._curr_location_x = int(self._prev_location_x + (x_screen - self._prev_location_x) / SMOOTHENING_RATIO)
-        self._curr_location_y = int(self._prev_location_y + (y_screen - self._prev_location_y) / SMOOTHENING_RATIO)
+        x_screen, y_screen = _screen_coordinates(index_finger[0], index_finger[1])
+        self._curr_location_x = int(self._prev_location_x + (x_screen - self._prev_location_x) / Config.SMOOTHENING_RATIO)
+        self._curr_location_y = int(self._prev_location_y + (y_screen - self._prev_location_y) / Config.SMOOTHENING_RATIO)
         ctypes.windll.user32.SetCursorPos(self._curr_location_x, self._curr_location_y)
         self._prev_location_x, self._prev_location_y = self._curr_location_x, self._curr_location_y
 
     def _process_clicks(self, fingers: list[int]) -> None:
         """Process mouse clicks (single, double, hold) based on predefined gestures / states"""
-        thumb_up, index_up, middle_up,  = fingers[0], fingers[1], fingers[2]
+        thumb_up, index_up, middle_up = fingers[0], fingers[1], fingers[2]
         current_time = time.time()
-        if index_up and middle_up and _is_cooldown_elapsed(self._last_click_time, CLICK_COOLDOWN):
+        if index_up and middle_up and _is_cooldown_elapsed(self._last_click_time, Config.CLICK_COOLDOWN):
             self._click.double()
             self._last_click_time = current_time
-        self._click.single_hold(thumb_up)
+        self._click.process_thumb_gesture(thumb_up)
 
     def detect_hand(self, img: np.ndarray) -> None:
         """Detect hand(s) and control mouse based on gestures"""
@@ -116,8 +140,9 @@ class VirtualMouse:
             # if index is up and the rest is down, except the thumb [for click mode]
             move_mode: bool = fingers[1] == 1 and all(x == 0 for x in fingers[2:])
 
-            cv2.rectangle(img, (FRAME_REDUCTION, FRAME_REDUCTION),
-                          (CAMERA_WIDTH - FRAME_REDUCTION, CAMERA_HEIGHT - FRAME_REDUCTION), (255, 0, 255), 2)
+            cv2.rectangle(img, (Config.FRAME_REDUCTION, Config.FRAME_REDUCTION),
+                          (Config.CAMERA_WIDTH - Config.FRAME_REDUCTION, Config.CAMERA_HEIGHT - Config.FRAME_REDUCTION),
+                          (255, 0, 255), 2)
 
             if move_mode:
                 self._move_mouse(index)
